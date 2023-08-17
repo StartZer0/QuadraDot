f0f3f30d819020307720ebb07b41aa15a67c95c0
StartZer0 <59214167+StartZer0@users.noreply.github.com>
2023-08-16 22:36:40 -0400

    Update Tile.cs

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -51,16 +51,13 @@ public class Tile : MonoBehaviour {
                 SetVisible();
             }
         }
-    }
-
-  
+    }  
 
     private int temp = 4;
     private int number;
     private Text TileText;
     private Image TileImage;
     private Animator anim;
-    
     
     public static int ChildCountActive(GameObject t) {
         int k = 0;
@@ -70,12 +67,8 @@ public class Tile : MonoBehaviour {
             {
                 k++; 
                 Debug.Log("K is " + k);
-
             }
-
         }
-
-
 
         return k;
     }
@@ -96,19 +89,6 @@ public class Tile : MonoBehaviour {
     {
         anim.SetTrigger("Appear");
     }
-
-
-
-    //void ApplyStyleFromHolder(int index) {
-    //    //if (index == 4)
-    //    //{
-    //    //    TileImage.enabled = false;
-    //    //}
-    //    TileText.text = TileStyleHolder.Instance.TileStyles[index].Number.ToString();
-    //    //TileText.color = TileStyleHolder.Instance.TileStyles[index].TextColor;
-    //    TileImage.color = TileStyleHolder.Instance.TileStyles[index].TileColor;
-       
-    //}
 
     void ApplyThisColorFromHolder(int index)
     {
@@ -137,81 +117,15 @@ public class Tile : MonoBehaviour {
             case 7:
                 ApplyThisColorFromHolder(5);
                 break;
-            //case 128:
-            //    ApplyThisColorFromHolder(6);
-            //    break;
-            //case 256:
-            //    ApplyThisColorFromHolder(7);
-            //    break;
-            //case 512:
-            //    ApplyThisColorFromHolder(8);
-            //    break;
-            //case 1024:
-            //    ApplyThisColorFromHolder(8);
-            //    break;
             default:
                 Debug.LogError("Check the numbers that you passed to ApplyStyle");
                 break;
         }
-
-    }
-
-    //void ApplyStyle(int num, int child ) {
-        
-    //    if (child == 4)
-    //    {
-    //            switch (num) {
-    //                case 2:
-    //                    ApplyStyleFromHolder(0);
-    //                    break;
-    //                case 4:
-    //                    ApplyStyleFromHolder(1);
-    //                    break;
-    //                case 8:
-    //                    ApplyStyleFromHolder(2);
-    //                    break;
-    //                case 16:
-    //                    ApplyStyleFromHolder(3);
-    //                    break;
-    //                case 32:
-    //                    ApplyStyleFromHolder(4);
-    //                    break;
-    //                case 64:
-    //                    ApplyStyleFromHolder(5);
-    //                    break;
-    //                case 128:
-    //                    ApplyStyleFromHolder(6);
-    //                    break;
-    //                case 256:
-    //                    ApplyStyleFromHolder(7);
-    //                    break;
-    //                case 512:
-    //                    ApplyStyleFromHolder(8);
-    //                    break;
-    //                case 1024:
-    //                    ApplyStyleFromHolder(9);
-    //                    break;
-    //                case 2048:
-    //                    ApplyStyleFromHolder(10);
-    //                    break;
-    //                case 4096:
-    //                    ApplyStyleFromHolder(11);
-    //                    break;
-    //                default:
-    //                Debug.LogError("Check the numbers that you passed to ApplyStyle");
-    //                break;
-    //        }
-    //    }
-        
-
-    //}
-
-    
+    }   
 
     private void SetVisible() {
         TileImage.enabled = true;
         TileText.enabled = false;
-
     }
 
     private void SetEmpty() {
