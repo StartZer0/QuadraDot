2615295e7a53bf159b543334c251fede785ae1f0
StartZer0 <59214167+StartZer0@users.noreply.github.com>
2023-08-16 22:38:56 -0400

    Update RadialBarProgressBlue.cs

--- Assets/Scripts/RadialBarProgressBlue.cs
+++ Assets/Scripts/RadialBarProgressBlue.cs
@@ -6,24 +6,14 @@ using UnityEngine.UI;
 
 public class RadialBarProgressBlue : MonoBehaviour
 {
-
     public Image ImgBar;
-    //public Text TxtProgress;
-    //private float WholeAmount = 60f;
     public float AmountD;
     public float fillAmountValue;
     public float numberText;
 
-
-    //[Range(0, 100)] public int Amount = 0;
-
-    // Use this for initialization
     void Awake() {
         AmountD = 100f;
         fillAmountValue = 100f;
-        //ImgBar = GameObject.Find("ImgBG").GetComponent<Image>();
-        //ImgBar.fillAmount = WholeAmount;
-
     }
 
     // Update is called once per frame
@@ -37,29 +27,10 @@ public class RadialBarProgressBlue : MonoBehaviour
             temp.x = AmountD / 100;
             temp.y = AmountD / 100;
             temp.z = AmountD / 100;
-
-            //AmountD -= Time.deltaTime;
-            //ImgBar.fillAmount = AmountD / WholeAmount;
             ImgBar.transform.localScale = temp;
             fillAmountValue = AmountD;
-            //CombatTextManager.Instance.CreateText(transform.position);
-            //fillAmountValue = ImgBar.fillAmount;
-            //TxtProgress.text = string.Format(" {0}%", Convert.ToInt32(AmountD / WholeAmount * 100));
         }
 
-        //1. + dlya togo chtobi uvelichit 
-        //    2. bolle maximuma
     }
-
-
-    //public void OnValidate()
-    //{
-    //    float amount = Amount / 100.0f;
-    //    ImgBar.fillAmount = amount;
-    //    TxtProgress.text = string.Format("{0}", Amount);
-    //}
-
-
-
 
 }
