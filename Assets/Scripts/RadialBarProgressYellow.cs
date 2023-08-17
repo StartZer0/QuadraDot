75ed3d9af13278ddf2bf8b7ae8391f25c52abfaf
StartZer0 <59214167+StartZer0@users.noreply.github.com>
2023-08-16 22:37:49 -0400

    Update RadialBarProgressYellow.cs

--- Assets/Scripts/RadialBarProgressYellow.cs
+++ Assets/Scripts/RadialBarProgressYellow.cs
@@ -7,20 +7,12 @@ using UnityEngine.UI;
 public class RadialBarProgressYellow : MonoBehaviour {
 
     public Image ImgBar;
-    //public Text TxtProgress;
-    //private float WholeAmount = 60f;
     public float AmountD;
     public float fillAmountValue;
 
-
-    //[Range(0, 100)] public int Amount = 0;
-
-    // Use this for initialization
     void Awake() {
         AmountD = 100f;
         fillAmountValue = 100f;
-        //ImgBar = GameObject.Find("ImgBG").GetComponent<Image>();
-        //ImgBar.fillAmount = WholeAmount;
 
     }
 
@@ -35,28 +27,10 @@ public class RadialBarProgressYellow : MonoBehaviour {
             temp.x = AmountD / 100;
             temp.y = AmountD / 100;
             temp.z = AmountD / 100;
-
-            //AmountD -= Time.deltaTime;
-            //ImgBar.fillAmount = AmountD / WholeAmount;
             ImgBar.transform.localScale = temp;
             fillAmountValue = AmountD;
-            //fillAmountValue = ImgBar.fillAmount;
-            //TxtProgress.text = string.Format(" {0}%", Convert.ToInt32(AmountD / WholeAmount * 100));
         }
 
-        //1. + dlya togo chtobi uvelichit 
-        //    2. bolle maximuma
     }
-
-
-    //public void OnValidate()
-    //{
-    //    float amount = Amount / 100.0f;
-    //    ImgBar.fillAmount = amount;
-    //    TxtProgress.text = string.Format("{0}", Amount);
-    //}
-
-
-
 
 }
